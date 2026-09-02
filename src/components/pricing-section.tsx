5bfe0f7678b821c8f9a678b64c2a3087b27582ed
Maxime Soulaine <maximesoulaine@MacBook-Pro-de-Maxime.local>
2026-09-02 13:29:11 +0200

    Revert "Fix: import useEffect"
    This reverts commit 89fc3449198a503f20c822c722d7be81f68e3c1d.

--- src/components/pricing-section.tsx
+++ src/components/pricing-section.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import { FadeIn } from "@/components/fade-in";
 import { Badge } from "@/components/ui/badge";
